3bc26c843bc6885362017d7a2ea6aa869ec96f83
Eugenio <eugenio.dzubanov@yandex.ru>
2019-07-10 22:27:23 +0300

    Крайний файл

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,15 +1,9 @@
 var reservButton = document.querySelector(".search-button");
 var popup = document.querySelector(".hotel-reservation");
 
+popup.classList.add("hotel-reservation-none");
+
 reservButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("hotel-reservation-none");
-  if (!popup.classList.contains("hotel-reservation-none")) {
-    evt.preventDefault();
-    popup.classList.add("reserv-form");
-  }
-  if (popup.classList.contains("hotel-reservation-none")) {
-    evt.preventDefault();
-    popup.classList.remove("reserv-form");
-      }
+  popup.classList.toggle("reserv-form");
 });
