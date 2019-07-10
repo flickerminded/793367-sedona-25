ae15a520d755317cc3b58da5ae32e0bf05cac954
Eugenio <eugenio.dzubanov@yandex.ru>
2019-07-10 19:39:31 +0300

    123

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -5,10 +5,10 @@ var dataDeparture = popup.querySelector("[name=departure]");
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (!popup.classList.contains("hidden")) {
+    if (!popup.classList.contains("visually-hidden")) {
       evt.preventDefault();
       popup.classList.remove("animation-reserv");
-      popup.classList.add("hidden");
+      popup.classList.add("visually-hidden");
       popup.classList.remove("incorrectly-put");
     }
   }
@@ -16,12 +16,12 @@ window.addEventListener("keydown", function (evt) {
 
 reservButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("hidden");
-  if (!popup.classList.contains("hidden")) {
+  popup.classList.toggle("visually-hidden");
+  if (!popup.classList.contains("visually-hidden")) {
     evt.preventDefault();
     popup.classList.add("animation-reserv");
   }
-  if (popup.classList.contains("hidden")) {
+  if (popup.classList.contains("visually-hidden")) {
     evt.preventDefault();
     popup.classList.remove("animation-reserv");
     popup.classList.remove("incorrectly-put");
