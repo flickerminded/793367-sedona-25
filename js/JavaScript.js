d01d578a141935d9806f73e9ad8923d8be30ecaa
Eugenio <eugenio.dzubanov@yandex.ru>
2019-07-10 22:07:53 +0300

    Не получается JS

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,7 +1,5 @@
 var reservButton = document.querySelector(".search-button");
 var popup = document.querySelector(".hotel-reservation");
-var arrival = popup.querySelector("[name=arrival]");
-var depart = popup.querySelector("[name=departure]");
 
 reservButton.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -14,15 +12,4 @@ reservButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("reserv-form");
       }
-});
-
-popup.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  if(!arrival.value||!depart.value) {
-    evt.preventDefault();
-        popup.offsetWidth = popup.offsetWidth;
-      }
-  else {
-    console.log("Отправить");
-  }
 });
